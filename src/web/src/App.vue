e025121fdf4863de0423082efe111c303125f3d8
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2024-11-30 16:06:41 -0500

    Cleanup App.vue

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -7,27 +7,3 @@
 import { RouterView } from 'vue-router'
 import SiteHeader from './components/shared/SiteHeader.vue'
 </script>
-
-<style lang="scss" scoped>
-@import './assets/variables.scss';
-
-.header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  &_left {
-    margin-right: auto;
-    color: $font-color-darker;
-  }
-
-  &_center {
-    color: $font-color-brighter;
-    font-size: larger;
-  }
-
-  &_right {
-    margin-left: auto;
-  }
-}
-</style>
