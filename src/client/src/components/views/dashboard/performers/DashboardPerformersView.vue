fd0b5b7625e8076692ea4841eeb5917278a4255b
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-02-08 10:47:14 -0500

    #12 Add grid to view existing performers

--- src/client/src/components/views/dashboard/performers/DashboardPerformersView.vue
+++ src/client/src/components/views/dashboard/performers/DashboardPerformersView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="performers">
     <NewPerformerForm />
+    <PerformersGrid />
   </div>
 </template>
 
 <script lang="ts" setup>
 import NewPerformerForm from './NewPerformerForm.vue'
+import PerformersGrid from './PerformersGrid.vue'
 </script>
--- src/client/src/components/views/dashboard/performers/PerformersGrid.vue
+++ src/client/src/components/views/dashboard/performers/PerformersGrid.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="performer-grid">
-    <DataTable></DataTable>
+    <DataTable v-if="isLoaded" :value="performers">
+      <Column field="name" header="Name" />
+      <Column field="url" header="URL" />
+      <Column field="modifiedDate" header="Last Modified">
+        <template #body="slotProps">
+          {{ new Date(slotProps.data.modifiedDate).toLocaleString() }}
+        </template>
+      </Column>
+    </DataTable>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { DataTable } from 'primevue'
+import api from '@/api/api'
+import type { PerformerResponse } from '@/api/performers/performerResponse'
+import { Column, DataTable } from 'primevue'
+import { onMounted, ref, type Ref } from 'vue'
+
+const isLoaded = ref(false)
+const performers: Ref<PerformerResponse[]> = ref([])
+
+onMounted(async () => {
+  const response = await api.performers.get()
+  isLoaded.value = true
+  if (!response.isSuccess()) {
+    return
+  }
+  performers.value = response.data!
+})
 </script>
