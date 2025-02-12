776256176f79417cd6667e82934dc0f86b842f25
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-02-12 00:11:04 -0500

    #17 Remove axios, sidetrack make dark toggle persist

--- src/client/src/components/controls/DarkModeToggle.vue
+++ src/client/src/components/controls/DarkModeToggle.vue
@@ -5,16 +5,27 @@
       :model-value="isDarkMode"
       @value-change="handleDarkModeToggle"
       class="dark-mode-toggle"
-    ></ToggleSwitch>
+    />
     <i class="pi pi-moon"></i>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { isTrueString } from '@/utils/booleanUtils'
+import { useLocalStorage } from '@vueuse/core'
 import { ToggleSwitch } from 'primevue'
-import { onMounted, ref, type Ref } from 'vue'
+import { computed, onMounted } from 'vue'
 
-const isDarkMode: Ref<boolean> = ref(true)
+const isDarkModeStored = useLocalStorage('isDarkMode', 'true')
+const toggleDarkModeStored = () => {
+  if (isTrueString(isDarkModeStored.value)) {
+    isDarkModeStored.value = 'false'
+  } else {
+    isDarkModeStored.value = 'true'
+  }
+}
+const isDarkMode = computed(() => isTrueString(isDarkModeStored.value))
+
 onMounted(() => {
   if (
     isDarkMode.value &&
@@ -29,8 +40,8 @@ onMounted(() => {
   }
 })
 
-const handleDarkModeToggle = (newValue: boolean) => {
-  isDarkMode.value = newValue
+const handleDarkModeToggle = () => {
+  toggleDarkModeStored()
   document.documentElement.classList.toggle('dark-mode-toggle')
   document.body.classList.toggle('dark-mode-toggle')
 }
--- src/client/src/components/views/dashboard/DashboardSchedulesTimeslotsTable.vue
+++ src/client/src/components/views/dashboard/DashboardSchedulesTimeslotsTable.vue
@@ -1,11 +1,11 @@
 <template>
-  <DataTable data-key="id" :value="schedule.timeslots">
+  <DataTable data-key="id" :value="rows">
     <Column field="start" header="Start">
       <template #body="timeslotProps">
-        {{ timeslotProps.data.start.toLocaleDateString() }}
+        {{ timeslotProps.data.start.toLocaleTimeString() }}
       </template>
     </Column>
-    <Column field="performer.name" header="Performer" />
+    <Column field="timeslot.performer.name" header="Performer" />
   </DataTable>
 </template>
 
@@ -13,7 +13,7 @@
 import { DataTable, Column } from 'primevue'
 import type { ScheduleResponse } from '@/api/schedules/scheduleResponse'
 import { hoursBetween } from '@/utils/dateUtils'
-import { ref } from 'vue'
+import { onMounted, ref, type Ref } from 'vue'
 import type { TimeslotResponse } from '@/api/schedules/timeslots/timeslotResponse'
 
 const props = defineProps<{
@@ -24,18 +24,24 @@ const startDate = ref(new Date(Date.parse(props.schedule.start)))
 const endDate = ref(new Date(Date.parse(props.schedule.end)))
 
 interface TimeslotRow {
-  startTime: Date
+  start: string
   timeslot?: TimeslotResponse
 }
 
+onMounted(() => {
+  setupRows()
+})
+
+const rows: Ref<TimeslotRow[]> = ref([])
 const setupRows = () => {
-  const rows: TimeslotRow[] = []
+  const newRows: TimeslotRow[] = []
   const hours = hoursBetween(startDate.value, endDate.value)
   hours.forEach((hour) => {
-    rows.push({
-      startTime: hour,
+    newRows.push({
+      start: hour.toLocaleTimeString(),
       timeslot: props.schedule.timeslots.find((timeslot) => timeslot.start === hour.toISOString())
     })
   })
+  rows.value = newRows
 }
 </script>
