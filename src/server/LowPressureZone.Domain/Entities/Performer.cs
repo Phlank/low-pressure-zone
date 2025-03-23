714e038c34a01013c28918a2859dc90c6df2812c
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-03-23 13:32:31 -0400

    #46 Make URL column optional

--- src/server/LowPressureZone.Domain/Entities/Performer.cs
+++ src/server/LowPressureZone.Domain/Entities/Performer.cs
@@ -3,9 +3,9 @@
 public sealed class Performer : BaseEntity
 {
     public required string Name { get; set; }
-    public required string Url { get; set; }
-    public required List<Guid> LinkedUserIds { get; set; } = [];
+    public required string? Url { get; set; }
+    public required ICollection<Guid> LinkedUserIds { get; set; } = [];
 
-    public List<Timeslot> Timeslots { get; set; } = [];
+    public ICollection<Timeslot> Timeslots { get; set; } = [];
     public bool IsDeleted { get; set; }
 }
---
+++ src/server/LowPressureZone.Domain/Migrations/20250323172710_MakePerformerUrlOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LowPressureZone.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePerformerUrlOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Performers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Performers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
