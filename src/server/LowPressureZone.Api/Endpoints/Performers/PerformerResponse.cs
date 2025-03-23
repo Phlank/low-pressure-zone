a025056ca35852a47b560d3ccb7c8735080cfb7e
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-03-23 13:46:21 -0400

    #46 Adjust other endpoint classes to handle null URLs

--- src/server/LowPressureZone.Api/Endpoints/Performers/PerformerResponse.cs
+++ src/server/LowPressureZone.Api/Endpoints/Performers/PerformerResponse.cs
@@ -4,7 +4,7 @@ public sealed class PerformerResponse
 {
     public required Guid Id { get; set; }
     public required string Name { get; set; }
-    public required string Url { get; set; }
+    public required string? Url { get; set; }
     public required bool IsDeletable { get; set; }
     public required bool IsEditable { get; set; }
     public required bool IsLinkableToTimeslot { get; set; }
