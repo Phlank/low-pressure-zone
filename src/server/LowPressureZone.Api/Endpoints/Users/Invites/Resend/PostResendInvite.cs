233960ab3ab9710aae9b718e7eee41b889eb80fa
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-03-28 15:22:30 -0400

    #78 Implement post resend invite endpoint

---
+++ src/server/LowPressureZone.Api/Endpoints/Users/Invites/Resend/PostResendInvite.cs
@@ -0,0 +1,50 @@
+﻿using FastEndpoints;
+using LowPressureZone.Api.Constants;
+using LowPressureZone.Api.Extensions;
+using LowPressureZone.Api.Services;
+using LowPressureZone.Identity;
+using LowPressureZone.Identity.Constants;
+using LowPressureZone.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace LowPressureZone.Api.Endpoints.Users.Invites.Resend;
+
+public class PostResendInvite(IdentityContext identityContext, UserManager<AppUser> userManager, EmailService emailService) : EndpointWithoutRequest
+{
+    private readonly DateTime _start = DateTime.UtcNow;
+
+    public override void Configure()
+    {
+        Post("/users/invites/resend/{id}");
+        Roles(RoleNames.Admin);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var id = Route<Guid>("id");
+        var invite = identityContext.Invitations.FirstOrDefault(invitation => invitation.Id == id);
+        if (invite == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        var user = await userManager.FindByIdAsync(invite.UserId.ToString());
+        if (user == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        var inviteToken = await userManager.GenerateUserTokenAsync(user, TokenProviders.Default, TokenPurposes.Invite);
+        var tokenContext = new TokenContext
+        {
+            Email = user.Email!,
+            Token = inviteToken
+        };
+        await emailService.SendInviteEmailAsync(user.Email!, tokenContext);
+        invite.LastSentDate = DateTime.UtcNow;
+        await identityContext.SaveChangesAsync(ct);
+        await this.SendDelayedNoContentAsync(_start, ct);
+    }
+}
