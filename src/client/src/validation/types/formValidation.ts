61ee2ee89af6e79a30f298844e17e9804f7c5d9d
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-03-31 08:04:21 -0400

    #78 Remove unnecessary assertion

--- src/client/src/validation/types/formValidation.ts
+++ src/client/src/validation/types/formValidation.ts
@@ -57,7 +57,7 @@ class FormValidationImplementation<TForm extends object> implements FormValidati
   public isValid = (...keys: (keyof TForm)[]) => {
     if (keys.length === 0) keys = this.getKeys()
     for (const key of keys) {
-      const isKeyValid = this.propertyState[key as keyof TForm].isValid.value
+      const isKeyValid = this.propertyState[key].isValid.value
       if (isKeyValid === false) return false
     }
     return true
