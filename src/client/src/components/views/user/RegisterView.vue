6cd4986c7d0162c052dd28cd85c712d7502133a9
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-04-25 14:20:32 -0400

    #105 Move resend invite to FormArea

--- src/client/src/components/views/user/RegisterView.vue
+++ src/client/src/components/views/user/RegisterView.vue
@@ -1,83 +1,79 @@
 <template>
   <SinglePanelViewWrapper>
-    <Panel
-      class="register single-panel-center"
-      header="Register User">
-      <FormArea>
-        <IftaFormField
-          :message="validation.message('username')"
-          input-id="usernameInput"
-          label="Username"
-          size="m">
-          <InputText
-            id="usernameInput"
-            v-model="formState.username"
-            :invalid="!validation.isValid('username')"
-            @update:model-value="validation.validateIfDirty('username')" />
-        </IftaFormField>
-        <IftaFormField
-          :message="validation.message('displayName')"
-          input-id="displayNameInput"
-          label="Display Name"
-          size="m">
-          <InputText
-            id="displayNameInput"
-            v-model="formState.displayName"
-            :invalid="!validation.isValid('displayName')"
-            @update:model-value="validation.validateIfDirty('displayName')" />
-        </IftaFormField>
-        <IftaFormField
-          :message="validation.message('password')"
-          input-id="passwordInput"
-          label="Password"
-          size="m">
-          <Password
-            id="passwordInput"
-            v-model="formState.password"
-            :feedback="false"
-            :invalid="!validation.isValid('password')"
-            type="password"
-            @update:model-value="validation.validateIfDirty('password')" />
-        </IftaFormField>
-        <FormField
-          input-id="passwordHelpTextMessage"
-          size="m">
-          <Message>
-            <div style="display: flex; flex-direction: column; justify-content: start">
-              <div>Password must meet the following requirements:</div>
-              <ul>
-                <li>At least 8 characters in length</li>
-                <li>At least one symbol</li>
-                <li>At least one uppercase letter</li>
-                <li>At least one lowercase letter</li>
-                <li>At least one digit</li>
-              </ul>
-            </div>
-          </Message>
-        </FormField>
-        <IftaFormField
-          :message="validation.message('confirmPassword')"
-          input-id="confirmPasswordInput"
-          label="Confirm Password"
-          size="m">
-          <Password
-            id="confirmPasswordInput"
-            v-model="formState.confirmPassword"
-            :feedback="false"
-            :invalid="!validation.isValid('confirmPassword')"
-            type="password"
-            @update:model-value="validation.validateIfDirty('confirmPassword')" />
-        </IftaFormField>
-        <template #actions>
-          <Button
-            :disabled="isSubmitting"
-            :loading="isSubmitting"
-            class="input"
-            label="Register"
-            @click="handleRegister" />
-        </template>
-      </FormArea>
-    </Panel>
+    <FormArea is-single-column>
+      <IftaFormField
+        :message="validation.message('username')"
+        input-id="usernameInput"
+        label="Username"
+        size="m">
+        <InputText
+          id="usernameInput"
+          v-model="formState.username"
+          :invalid="!validation.isValid('username')"
+          @update:model-value="validation.validateIfDirty('username')" />
+      </IftaFormField>
+      <IftaFormField
+        :message="validation.message('displayName')"
+        input-id="displayNameInput"
+        label="Display Name"
+        size="m">
+        <InputText
+          id="displayNameInput"
+          v-model="formState.displayName"
+          :invalid="!validation.isValid('displayName')"
+          @update:model-value="validation.validateIfDirty('displayName')" />
+      </IftaFormField>
+      <IftaFormField
+        :message="validation.message('password')"
+        input-id="passwordInput"
+        label="Password"
+        size="m">
+        <Password
+          id="passwordInput"
+          v-model="formState.password"
+          :feedback="false"
+          :invalid="!validation.isValid('password')"
+          type="password"
+          @update:model-value="validation.validateIfDirty('password')" />
+      </IftaFormField>
+      <FormField
+        input-id="passwordHelpTextMessage"
+        size="m">
+        <Message>
+          <div style="display: flex; flex-direction: column; justify-content: start">
+            <div>Password must meet the following requirements:</div>
+            <ul>
+              <li>At least 8 characters in length</li>
+              <li>At least one symbol</li>
+              <li>At least one uppercase letter</li>
+              <li>At least one lowercase letter</li>
+              <li>At least one digit</li>
+            </ul>
+          </div>
+        </Message>
+      </FormField>
+      <IftaFormField
+        :message="validation.message('confirmPassword')"
+        input-id="confirmPasswordInput"
+        label="Confirm Password"
+        size="m">
+        <Password
+          id="confirmPasswordInput"
+          v-model="formState.confirmPassword"
+          :feedback="false"
+          :invalid="!validation.isValid('confirmPassword')"
+          type="password"
+          @update:model-value="validation.validateIfDirty('confirmPassword')" />
+      </IftaFormField>
+      <template #actions>
+        <Button
+          :disabled="isSubmitting"
+          :loading="isSubmitting"
+          class="input"
+          label="Register"
+          @click="handleRegister" />
+      </template>
+    </FormArea>
   </SinglePanelViewWrapper>
 </template>
 
@@ -89,7 +85,7 @@ import { useAuthStore } from '@/stores/authStore'
 import { registerRequestRules } from '@/validation/requestRules'
 import { createFormValidation } from '@/validation/types/formValidation'
 import { onKeyDown } from '@vueuse/core'
-import { Button, InputText, Message, Panel, Password, useToast } from 'primevue'
+import { Button, InputText, Message, Password, useToast } from 'primevue'
 import { onMounted, reactive, ref } from 'vue'
 import authApi, { type RegisterRequest } from '@/api/resources/authApi.ts'
 import tryHandleUnsuccessfulResponse from '@/api/tryHandleUnsuccessfulResponse.ts'
