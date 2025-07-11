1f6c7cb99f135b608c29629cb65bc24fe548df06
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-07-11 15:46:20 -0400

    #260 Add column, add streamer method to azuracast client

---
+++ src/server/LowPressureZone.Identity/Migrations/20250711194208_AddStreamerId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LowPressureZone.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStreamerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StreamerId",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StreamerId",
+                table: "AspNetUsers");
+        }
+    }
+}
