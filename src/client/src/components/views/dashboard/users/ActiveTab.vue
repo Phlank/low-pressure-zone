1b4167ee98b9df5ae86685e12a0112999b634025
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-07-13 08:51:48 -0400

    #261 Fix lint issue

--- src/client/src/components/views/dashboard/users/ActiveTab.vue
+++ src/client/src/components/views/dashboard/users/ActiveTab.vue
@@ -8,7 +8,6 @@
 import UsersGrid from '@/components/views/dashboard/users/UsersGrid.vue'
 import { useToast } from 'primevue'
 import usersApi, { type UserResponse } from '@/api/resources/usersApi.ts'
-import { showCreateSuccessToast } from '@/utils/toastUtils.ts'
 import tryHandleUnsuccessfulResponse from '@/api/tryHandleUnsuccessfulResponse.ts'
 import { onMounted, ref, type Ref } from 'vue'
 
@@ -22,18 +21,4 @@ onMounted(async () => {
   }
   users.value = userResponse.data()
 })
-
-const showConfirmDialog = ref(false)
-const isSubmittingConfirmDialog = ref(false)
-const handleCreateStreamers = async () => {
-  isSubmittingConfirmDialog.value = true
-  const response = await usersApi.createStreamers()
-  if (response.isSuccess()) {
-    showCreateSuccessToast(toast, 'Streamers', 'All')
-  } else {
-    tryHandleUnsuccessfulResponse(response, toast)
-  }
-  showConfirmDialog.value = false
-  isSubmittingConfirmDialog.value = false
-}
 </script>
