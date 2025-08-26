8abfffd4ad29a34cfac5c0f0b3bdc9589bb67057
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-08-26 15:49:25 -0400

    #297 Finish updating relationships form

--- src/client/src/components/views/dashboard/communities/CommunityRelationshipsGrid.vue
+++ src/client/src/components/views/dashboard/communities/CommunityRelationshipsGrid.vue
@@ -80,42 +80,39 @@
         <template #actions></template>
       </FormArea>
     </div>
-    <FormDialog
-      :is-submitting="isSubmitting"
-      :visible="showCreateDialog"
+    <Dialog
+      v-model:visible="showCreateDialog"
+      :draggable="false"
       header="Create Relationship"
-      @close="showCreateDialog = false"
-      @save="handleCreateDialogSave()">
+      modal>
       <CommunityRelationshipForm
-        ref="createForm"
-        :available-users="availableUsers" />
-    </FormDialog>
-    <FormDialog
-      :is-submitting="isSubmitting"
-      :visible="showUpdateDialog"
+        :available-users="availableUsers"
+        :community-id="community.id"
+        align-actions="right"
+        @after-submit="showCreateDialog = false" />
+    </Dialog>
+    <Dialog
+      v-model:visible="showUpdateDialog"
+      :draggable="false"
       header="Update Relationship"
-      @close="showUpdateDialog = false"
-      @save="handleEditDialogSave()">
+      modal>
       <CommunityRelationshipForm
-        ref="updateForm"
         :available-users="[userStore.getUser(updatingUserId)]"
-        :initial-state="updatingRelationship" />
-    </FormDialog>
+        :community-id="community.id"
+        :initial-state="updatingRelationship"
+        align-actions="right"
+        @after-submit="showUpdateDialog = false" />
+    </Dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Button, Column, DataTable, DataView, Divider, useToast } from 'primevue'
-import communityRelationshipsApi, {
-  type CommunityRelationshipResponse
-} from '@/api/resources/communityRelationshipsApi.ts'
-import { computed, type ComputedRef, inject, onMounted, ref, type Ref, useTemplateRef } from 'vue'
+import { Button, Column, DataTable, DataView, Dialog, Divider } from 'primevue'
+import { type CommunityRelationshipResponse } from '@/api/resources/communityRelationshipsApi.ts'
+import { computed, type ComputedRef, inject, onMounted, ref, type Ref } from 'vue'
 import type { UserResponse } from '@/api/resources/usersApi.ts'
-import FormDialog from '@/components/dialogs/FormDialog.vue'
 import CommunityRelationshipForm from '@/components/form/requestForms/CommunityRelationshipForm.vue'
 import { type CommunityResponse } from '@/api/resources/communitiesApi.ts'
-import tryHandleUnsuccessfulResponse from '@/api/tryHandleUnsuccessfulResponse.ts'
-import { showCreateSuccessToast, showEditSuccessToast } from '@/utils/toastUtils.ts'
 import GridActions from '@/components/data/grid-actions/GridActions.vue'
 import { useUserStore } from '@/stores/userStore.ts'
 import ListItem from '@/components/data/ListItem.vue'
@@ -124,7 +121,6 @@ import { useCommunityStore } from '@/stores/communityStore.ts'
 import { mobilePaginatorTemplate } from '@/constants/componentTemplates.ts'
 
 const isMobile: Ref<boolean> | undefined = inject('isMobile')
-const toast = useToast()
 const userStore = useUserStore()
 const communityStore = useCommunityStore()
 
@@ -151,8 +147,6 @@ const getMobileRelationshipText = (relationship: CommunityRelationshipResponse) 
   return roles.join(', ')
 }
 
-const createForm = useTemplateRef('createForm')
-const isSubmitting: Ref<boolean> = ref(false)
 const showCreateDialog: Ref<boolean> = ref(false)
 
 const handleAddUserClick = async () => {
@@ -160,38 +154,6 @@ const handleAddUserClick = async () => {
   showCreateDialog.value = true
 }
 
-const handleCreateDialogSave = async () => {
-  if (!createForm.value) return
-  const formState = createForm.value.formState
-  const validation = createForm.value.validation
-
-  const isValid = validation.validate()
-  if (!isValid) return
-
-  isSubmitting.value = true
-  const response = await communityRelationshipsApi.put(
-    props.community.id,
-    createForm.value.formState.userId,
-    createForm.value.formState
-  )
-  isSubmitting.value = false
-
-  if (tryHandleUnsuccessfulResponse(response, toast)) return
-
-  const displayName = userStore.getUser(formState.userId).displayName
-  showCreateSuccessToast(toast, 'relationship', displayName)
-  showCreateDialog.value = false
-  communityStore.addRelationship(props.community.id, {
-    userId: formState.userId,
-    communityId: props.community.id,
-    displayName: userStore.getUser(formState.userId).displayName,
-    isOrganizer: formState.isOrganizer,
-    isPerformer: formState.isPerformer,
-    isEditable: true
-  })
-}
-
-const updateForm = useTemplateRef('updateForm')
 const showUpdateDialog = ref(false)
 const updatingRelationship: Ref<CommunityRelationshipResponse | undefined> = ref(undefined)
 const updatingUserId: Ref<string> = ref('')
@@ -200,31 +162,5 @@ const handleEditActionClick = (relationship: CommunityRelationshipResponse) => {
   updatingRelationship.value = relationship
   updatingUserId.value = relationship.userId
   showUpdateDialog.value = true
-}
-
-const handleEditDialogSave = async () => {
-  if (!updateForm.value) return
-  const isValid = updateForm.value.validation.validate()
-  if (!isValid) return
-
-  isSubmitting.value = true
-  const response = await communityRelationshipsApi.put(
-    props.community.id,
-    updateForm.value.formState.userId,
-    updateForm.value.formState
-  )
-  isSubmitting.value = false
-
-  if (tryHandleUnsuccessfulResponse(response, toast)) return
-
-  const displayName = userStore.getUser(updatingUserId.value).displayName
-  showEditSuccessToast(toast, 'relationship', displayName)
-  showUpdateDialog.value = false
-  await communityStore.updateRelationshipAsync(
-    updatingRelationship.value!.communityId,
-    updatingRelationship.value!.userId
-  )
-  updatingRelationship.value = undefined
-  updatingUserId.value = ''
 }
 </script>
