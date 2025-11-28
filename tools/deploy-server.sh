f175c1f4a2fe8b66fa3d9490e6643566958c4a91
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2025-11-28 16:27:04 -0500

    #363 Use yarn install --ignore-scripts in client deploy

--- tools/deploy-server.sh
+++ tools/deploy-server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 sudo service lpzapi stop
-cd src/server/LowPressureZone.Api
+cd src/server/LowPressureZone.Api || return
 rm Properties/launchSettings.json
 cp Properties/launchSettings-production.json Properties/launchSettings.json
 dotnet publish LowPressureZone.Api.csproj -c Release -v diag
-cd bin/Release/net9.0
+cd bin/Release/net9.0 || return
 sudo rm -r /var/www/html/low-pressure-zone-api/* || true
 sudo cp -r * /var/www/html/low-pressure-zone-api
 sudo chown -R root:www-data /var/www/html/low-pressure-zone-api/*
 sudo chmod -R 650 /var/www/html/low-pressure-zone-api/*
 sudo service lpzapi start
 cd ../../../../..
-	
